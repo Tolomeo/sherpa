748157209aafa4d55f014239b181dc65c1c21b35
Diego Frattini <dgfrattini@gmail.com>
2021-08-23 00:36:36 +0100

    Path using H2 for title

--- src/path/Path.tsx
+++ src/path/Path.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { PopulatedPath } from '../../data'
-import { Timeline } from '../shared'
+import { Timeline, H2, P } from '../shared'
 import styles from './Path.module.scss'
 
 type Props = {
@@ -14,7 +14,7 @@ const Path = ({ path }: Props) => {
             <Timeline.Item key={resource.url}>
                 <a href={resource.url} target="_blank" rel="noreferrer">
                     <p className={styles.resource__source}>{resource.source}</p>
-                    <h2 className={styles.resource__title}>{resource.title}</h2>
+                    <H2>{resource.title}</H2>
                     <p className={styles.resource__type}>{resource.type}</p>
                 </a>
             </Timeline.Item>)}
