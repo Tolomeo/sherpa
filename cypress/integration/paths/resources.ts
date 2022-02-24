910a57b6465026262e7be7a4b1773b5b773e0d9d
Diego Frattini <dgfrattini@gmail.com>
2022-02-24 19:05:05 +0000

    Javascript and Typescript resources tests

--- cypress/integration/paths/resources.ts
+++ cypress/integration/paths/resources.ts
@@ -4,10 +4,10 @@ describe('Path resources', () => {
   // Object.entries(paths).forEach(([_, serialisedPath]) => {
   Object.entries({
     // htmlcss: paths.htmlcss,
-    webaccessibility: paths.webaccessibility,
-    /* javascript: paths.javascript,
+    // webaccessibility: paths.webaccessibility,
+    // javascript: paths.javascript,
     typescript: paths.typescript,
-    react: paths.react,
+    /* react: paths.react,
     reacttypescript: paths.reacttypescript,
     next: paths.next,
     node: paths.node,
