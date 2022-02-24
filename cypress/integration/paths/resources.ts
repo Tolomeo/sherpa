f01294a92de24a1bd777e7b5265aef218f40dea1
Diego Frattini <dgfrattini@gmail.com>
2022-02-24 18:13:36 +0000

    WebAccessibility resources tests

--- cypress/integration/paths/resources.ts
+++ cypress/integration/paths/resources.ts
@@ -2,7 +2,18 @@ import { paths, deserializePath } from '../../../data'
 
 describe('Path resources', () => {
   // Object.entries(paths).forEach(([_, serialisedPath]) => {
-  Object.entries({ htmlcss: paths.htmlcss }).forEach(([_, serialisedPath]) => {
+  Object.entries({
+    // htmlcss: paths.htmlcss,
+    webaccessibility: paths.webaccessibility,
+    /* javascript: paths.javascript,
+    typescript: paths.typescript,
+    react: paths.react,
+    reacttypescript: paths.reacttypescript,
+    next: paths.next,
+    node: paths.node,
+    git: paths.git,
+    neovim: paths.neovim, */
+  }).forEach(([_, serialisedPath]) => {
     const path = deserializePath(serialisedPath)
 
     describe('Path resources', () => {
