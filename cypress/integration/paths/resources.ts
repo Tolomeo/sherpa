db0385f881c042434146874f58d42680f79f203a
Diego Frattini <dgfrattini@gmail.com>
2022-02-25 18:27:07 +0000

    Nextjs resources tests

--- cypress/integration/paths/resources.ts
+++ cypress/integration/paths/resources.ts
@@ -8,9 +8,9 @@ describe('Resources', () => {
     // javascript: paths.javascript,
     // typescript: paths.typescript,
     // react: paths.react,
-    reacttypescript: paths.reacttypescript,
-    /* next: paths.next,
-    node: paths.node,
+    // reacttypescript: paths.reacttypescript,
+    next: paths.next,
+    /* node: paths.node,
     git: paths.git,
     neovim: paths.neovim, */
   }).forEach(([_, serialisedPath]) => {
