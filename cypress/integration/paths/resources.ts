9306041c6ea09859c768936749e7e3c0cd104f35
Diego Frattini <dgfrattini@gmail.com>
2022-02-25 20:07:47 +0000

    Health-check workflow

--- cypress/integration/paths/resources.ts
+++ cypress/integration/paths/resources.ts
@@ -1,19 +1,7 @@
 import { paths, deserializePath } from '../../../data'
 
 describe('Resources', () => {
-  // Object.entries(paths).forEach(([_, serialisedPath]) => {
-  Object.entries({
-    // htmlcss: paths.htmlcss,
-    // webaccessibility: paths.webaccessibility,
-    // javascript: paths.javascript,
-    // typescript: paths.typescript,
-    // react: paths.react,
-    // reacttypescript: paths.reacttypescript,
-    // next: paths.next,
-    // node: paths.node,
-    // git: paths.git,
-    neovim: paths.neovim,
-  }).forEach(([_, serialisedPath]) => {
+  Object.entries(paths).forEach(([_, serialisedPath]) => {
     const path = deserializePath(serialisedPath)
 
     describe(`"${path.title}" path resources`, () => {
