d63e7ae4ad8d2aa30b67f81523307cc41326aceb
Diego Frattini <dgfrattini@gmail.com>
2022-05-11 20:00:21 +0100

    Underline styles improvements

--- src/theme/components/Underline.tsx
+++ src/theme/components/Underline.tsx
@@ -1,7 +1,16 @@
 import { styled } from '@mui/material/styles'
 
+/* https://css-tricks.com/styling-underlines-web/#aa-background-image */
 const Underline = styled('u')`
-  text-decoration: underline currentColor dashed from-font;
+  text-decoration: none;
+  background-image: linear-gradient(
+    to right,
+    currentColor 40%,
+    transparent 40%
+  );
+  background-position: 0 1.175em;
+  background-repeat: repeat-x;
+  background-size: 0.25em 0.05em;
 `
 
 export default Underline
