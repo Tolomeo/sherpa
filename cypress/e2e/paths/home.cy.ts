4376ccc168c10e6caebfb1de2ac7c76c7f4761c1
Diego Frattini <dgfrattini@gmail.com>
2023-09-17 23:59:54 +0100

    Index E2E test replaced with smoke testing

--- cypress/e2e/paths/home.cy.ts
+++ cypress/e2e/paths/home.cy.ts
@@ -1,55 +1,5 @@
-import { paths, alternateSources } from '../../../data'
-
 describe('Index page', () => {
-  before(() => {
+  it('Renders with no errors', () => {
     cy.visit(Cypress.config('baseUrl')!)
-  })
-
-  describe('Paths list', () => {
-    beforeEach(() => {
-      cy.get('[data-testid="paths.list"]')
-        .find(
-          '[data-testid="paths.list.item"] a[data-testid="paths.list.item.link"]',
-        )
-        .as('pathLinks')
-    })
-
-    it('Renders a list of all the available paths links', () => {
-      cy.get('@pathLinks').should('have.length', Object.keys(paths).length)
-
-      Object.entries(paths).forEach(([pathKey, path], pathIndex) => {
-        const pathUrl = `/paths/${pathKey}`
-        const pathLinkText = `The ${path.title} path`
-
-        cy.get('@pathLinks')
-          .eq(pathIndex)
-          .should('have.attr', 'href', pathUrl)
-          .should('have.text', pathLinkText)
-      })
-    })
-  })
-
-  describe('Alternate sources list', () => {
-    beforeEach(() => {
-      cy.get('[data-testid="alternatives.list"]')
-        .find(
-          '[data-testid="alternatives.list.item"] a[data-testid="alternatives.list.item.link"]',
-        )
-        .as('alternativeLinks')
-    })
-
-    it('Renders a list of all the alternative sources links', () => {
-      cy.get('@alternativeLinks').should(
-        'have.length',
-        Object.keys(alternateSources).length,
-      )
-
-      Object.entries(alternateSources).forEach(([_, source], sourceIndex) => {
-        cy.get('@alternativeLinks')
-          .eq(sourceIndex)
-          .should('have.attr', 'href', source.url)
-          .should('have.text', source.title)
-      })
-    })
   })
 })
