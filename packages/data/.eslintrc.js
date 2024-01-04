14afef9c68f28a21901fdb33b6087854dd0b00d9
Diego Frattini <dgfrattini@gmail.com>
2024-01-04 09:49:30 +0000

    Initial monorepo setup

--- packages/data/.eslintrc.js
+++ packages/data/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   extends: ['sherpa/lib'],
-  ignorePatterns: ['types/**', 'dist/**'],
+  ignorePatterns: ['dist/**'],
   rules: {
     '@typescript-eslint/no-var-requires': 'off',
   },
