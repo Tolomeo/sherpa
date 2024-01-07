30baa060ca167d9b56706d0cfeddb17e58bfb4e1
Diego Frattini <dgfrattini@gmail.com>
2024-01-07 00:16:07 +0000

    Initial monorepo setup

--- apps/web/cypress.config.ts
+++ apps/web/cypress.config.ts
@@ -1,6 +1,8 @@
+// TODO: Try to fix once the data package compiles paths at build time
+/* eslint-disable import/no-unresolved -- Cypress compiles this file, breaking the following imports form the data package */
 import { defineConfig } from 'cypress'
-import * as pathsReadUtils from '@sherpa/data/paths/read'
-import * as resourcesReadUtils from '@sherpa/data/resources/read'
+import * as pathsReadUtils from '@sherpa/data/paths/read.js'
+import * as resourcesReadUtils from '@sherpa/data/resources/read.js'
 
 export default defineConfig({
   video: false,
