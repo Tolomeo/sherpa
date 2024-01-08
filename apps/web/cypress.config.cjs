4c6ddc480d0de5aab5eb79a9c2c3029974725f66
Diego Frattini <dgfrattini@gmail.com>
2024-01-08 11:38:23 +0000

    Initial monorepo setup

---
+++ apps/web/cypress.config.cjs
@@ -0,0 +1,24 @@
+const { defineConfig } = require('cypress')
+const pathsReadUtils = require('@sherpa/data/paths/read.js')
+const resourcesReadUtils = require('@sherpa/data/resources/read.js')
+
+export default defineConfig({
+  video: false,
+  chromeWebSecurity: false,
+  e2e: {
+    setupNodeEvents(on, _config) {
+      on('task', {
+        ...pathsReadUtils,
+        ...resourcesReadUtils,
+      })
+    },
+    baseUrl: 'http://localhost:3000',
+  },
+
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack',
+    },
+  },
+})
