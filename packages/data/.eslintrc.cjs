75ce9f77aae61a13b063fa77efaadadcdda5e67b
Diego Frattini <dgfrattini@gmail.com>
2024-01-28 18:45:11 +0000

    Data linting tweaks

--- packages/data/.eslintrc.cjs
+++ packages/data/.eslintrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
   extends: [require.resolve('@sherpa/configs/eslintrc.lib')],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   ignorePatterns: ['dist/**'],
   rules: {
     '@typescript-eslint/no-var-requires': 'off',
