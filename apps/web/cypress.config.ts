c3ecb6dc5bbccf19aef2ee1a168cee98b6cc1e0c
Diego Frattini <dgfrattini@gmail.com>
2024-02-02 13:40:53 +0000

    Cy config tweaks

--- apps/web/cypress.config.ts
+++ apps/web/cypress.config.ts
@@ -4,19 +4,6 @@ export default defineConfig({
   video: false,
   chromeWebSecurity: false,
   e2e: {
-    /* setupNodeEvents(on, _config) {
-      on('task', {
-        ...pathsReadUtils,
-        ...resourcesReadUtils,
-      })
-    }, */
     baseUrl: 'http://localhost:3000',
-  },
-
-  component: {
-    devServer: {
-      framework: 'next',
-      bundler: 'webpack',
-    },
   },
 })
