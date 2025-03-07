b0a2780bfd9c2b95d8c08341283aba9ec4e29152
Diego Frattini <dgfrattini@gmail.com>
2025-03-07 21:41:03 +0000

    Feat/Custom title scraper (#105)
    * Scripts tweaks
    
    * Scraper custom title rules
    
    * Data eslint config js overrides
    
    * Data tsconfig tweaks
    
    * Format fix

---
+++ packages/data/src/healthcheck/scraper/title.js
@@ -0,0 +1,23 @@
+// This file is js instead of ts because metascraper is not coded in TS, it just offers .d.ts definition files
+// This is a modified version of metascraper-title which prioritises <title/> over other rules
+import { $jsonld, $filter, title, toRule } from '@metascraper/helpers'
+
+const toTitle = toRule(title)
+
+export default () => {
+  const rules = {
+    title: [
+      toTitle(($) => $filter($, $('title'))),
+      toTitle(($) => $('meta[property="og:title"]').attr('content')),
+      toTitle(($) => $('meta[name="twitter:title"]').attr('content')),
+      toTitle(($) => $('meta[property="twitter:title"]').attr('content')),
+      toTitle($jsonld('headline')),
+      toTitle(($) => $filter($, $('.post-title'))),
+      toTitle(($) => $filter($, $('.entry-title'))),
+      toTitle(($) => $filter($, $('h1[class*="title" i] a'))),
+      toTitle(($) => $filter($, $('h1[class*="title" i]'))),
+    ],
+  }
+
+  return rules
+}
